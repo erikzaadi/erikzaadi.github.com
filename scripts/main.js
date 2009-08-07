a904f00b717a2a2e69fb2f5ca7db7f668a44465b
erikzaadi <erik.zaadi@gmail.com>
2009-08-07 19:00:11 +0300

    New Layout, including posts

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,36 @@
-$(document).ready(function(){
-	var user = "erikzaadi";
-	$("#GithubBadge").GitHubBadge(user);
-	InitTwitter(user);
+$(document).ready(function() {
+    $(".BreadCrumbs li.ui-state-default").hover(function() {
+        fadeAndChange($(this), true);
+    },
+		function() {
+		    fadeAndChange($(this), false);
+		}).fadeTo('fast', 0.8);
+    //Separate things that load scripts..
+    setTimeout(function() {
+        var user = "erikzaadi";
+        $('#switcher').themeswitcher();
+        $("#GithubBadge").GitHubBadge(user);
+        InitTwitter(user);
+    }, 25);
+
 });
 
- function InitTwitter(user) {
-        var url = "http://twitter.com/status/user_timeline/" + user + ".json?count=1&callback=?";
-        $.getJSON(url, GotTwitter);
-    }
-    function GotTwitter(data) {
-        if (!data || !data[0])
-            return;
-        var tweetDate = new Date(Date.parse(data[0].created_at.replace(" +0000", "")));
-        var followers = data[0].user.followers_count;
-        $("#Twitter").addClass("TwitterFollowerLink").append('<div class="TwitterFollowerDiv"><a class="TwitterFollowerLink" href="http://twitter.com/erikzaadi" alt="Twitter"><div><span class="TwitterFollowerSpanContainer"><span class="TwitterFollowerSpanAmount">' + followers + '</span>&nbsp;followers</span></div></a><div class="TwitterFollowerBy">by znag</div></div>');
-        tweetDate.setHours(tweetDate.getHours() + 3);
-        var msg = '<div class="TwitterToolTip">' +
+function fadeAndChange($elem, toggle) {
+    $($elem).stop().toggleClass("ui-state-hover", toggle).fadeTo('slow', toggle ? 1.0 : 0.8);
+}
+
+function InitTwitter(user) {
+    var url = "http://twitter.com/status/user_timeline/" + user + ".json?count=1&callback=?";
+    $.getJSON(url, GotTwitter);
+}
+function GotTwitter(data) {
+    if (!data || !data[0])
+        return;
+    var tweetDate = new Date(Date.parse(data[0].created_at.replace(" +0000", "")));
+    var followers = data[0].user.followers_count;
+    $("#Twitter").addClass("TwitterFollowerLink").append('<div class="TwitterFollowerDiv"><a class="TwitterFollowerLink" href="http://twitter.com/erikzaadi" alt="Twitter"><div><span class="TwitterFollowerSpanContainer"><span class="TwitterFollowerSpanAmount">' + followers + '</span>&nbsp;followers</span></div></a><div class="TwitterFollowerBy">by znag</div></div>');
+    tweetDate.setHours(tweetDate.getHours() + 3);
+    var msg = '<div class="TwitterToolTip">' +
         '<div class="TwitterTitle">' +
         tweetDate.toString() +
         '</div>' +
@@ -23,12 +38,12 @@ $(document).ready(function(){
         data[0].text +
         '</div>' +
         '</div>';
-        $("#Twitter").tooltip({
-            fade: 500,
-            delay: 0,
-            showURL: false,
-            bodyHandler: function() {
-                return $(msg);
-            }
-        });
-    }
+    $("#Twitter").tooltip({
+        fade: 500,
+        delay: 0,
+        showURL: false,
+        bodyHandler: function() {
+            return $(msg);
+        }
+    });
+}
